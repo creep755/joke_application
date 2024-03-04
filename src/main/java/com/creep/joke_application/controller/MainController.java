836f56608827c2ab509e248518e7c7de34b714c6
creep755 <mikhailovski.aleksandr@gmail.com>
2024-03-05 02:09:56 +0300

    fix fixed fix

--- src/main/java/com/creep/joke_application/controller/MainController.java
+++ src/main/java/com/creep/joke_application/controller/MainController.java
@@ -1,20 +1,22 @@
 package com.creep.joke_application.controller;
 
 import com.creep.joke_application.service.MainService;
-import lombok.AllArgsConstructor;
+
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @ControllerAdvice
 @RequestMapping("/api/v1")
-@AllArgsConstructor
 public class MainController {
 
     private final MainService mainService;
+
+    public MainController(MainService mainService) {
+        this.mainService = mainService;
+    }
 
     @GetMapping("/{action}")
     public String getRandomJoke(@PathVariable String action) {
         return mainService.getRandomJoke(action);
     }
-
 }
--- src/main/java/com/creep/joke_application/service/MainService.java
+++ src/main/java/com/creep/joke_application/service/MainService.java
@@ -1,21 +1,22 @@
 package com.creep.joke_application.service;
 
-import lombok.AllArgsConstructor;
+
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
 @Service
-@AllArgsConstructor
 public class MainService {
 
-    private static final String JOKE_API_URL = "https://official-joke-api.appspot.com/jokes/{action}";
+    private static final String JOKE_API_URL = "https://official-joke-api.appspot.com/jokes/";
 
     private final RestTemplate restTemplate;
 
-    public String getRandomJoke(String action) {
-
-        String url = JOKE_API_URL.replace("{action}",action);
-        return restTemplate.getForObject(url, String.class);
+    public MainService(RestTemplate restTemplate) {
+        this.restTemplate = restTemplate;
     }
 
+    public String getRandomJoke(String action) {
+        String url = JOKE_API_URL + action;
+        return restTemplate.getForObject(url, String.class);
+    }
 }
